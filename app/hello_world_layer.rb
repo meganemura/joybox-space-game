3de2280e28dcde44008af1d28c79b4335cea053d
meganemura <mura2megane@gmail.com>
2013-11-03 00:11:07 +0900

    Adding Parallax Scrolling

--- app/hello_world_layer.rb
+++ app/hello_world_layer.rb
@@ -10,5 +10,35 @@ class HelloWorldLayer < Joybox::Core::Layer
     @ship = Sprite.new(:frame_name => "SpaceFlier_sm_1.png")
     @ship.position = [Screen.width * 0.1, Screen.height * 0.5]
     @batch_node.add_child(@ship, :z => 1)
+
+
+    # 1) Create the CCParallaxNode
+    @background_node = CCParallaxNode.node
+    self << @background_node
+
+    # 2) Create the sprites we'll add to the CCParallaxNode
+    @spacedust_1    = Sprite.new(:file_name => "Backgrounds/bg_front_spacedust.png")
+    @spacedust_2    = Sprite.new(:file_name => "Backgrounds/bg_front_spacedust.png")
+    @planetsunrise  = Sprite.new(:file_name => "Backgrounds/bg_planetsunrise.png")
+    @galaxy         = Sprite.new(:file_name => "Backgrounds/bg_galaxy.png")
+    @spacialnomaly  = Sprite.new(:file_name => "Backgrounds/bg_spacialanomaly.png")
+    @spacialnomaly2 = Sprite.new(:file_name => "Backgrounds/bg_spacialanomaly2.png")
+
+    # 3) Determine relative movement speeds for space dust and background
+    dust_speed = [0.1, 0.1]
+    bg_speed = [0.05, 0.05]
+
+    # 4) Add children to CCParallaxNode
+    @background_node.addChild(@spacedust_1, z: 0, parallaxRatio: dust_speed, positionOffset: [0, Screen.half_height])
+    @background_node.addChild(@spacedust_2, z: 0, parallaxRatio: dust_speed, positionOffset: [@spacedust_1.contentSize.width, Screen.half_height])
+    @background_node.addChild(@galaxy, z: -1, parallaxRatio: bg_speed, positionOffset: [0, Screen.height * 0.7])
+    @background_node.addChild(@planetsunrise, z: -1, parallaxRatio: bg_speed, positionOffset: [600, 0])
+    @background_node.addChild(@spacialnomaly, z: -1, parallaxRatio: bg_speed, positionOffset: [900, Screen.height * 0.3])
+    @background_node.addChild(@spacialnomaly2, z: -1, parallaxRatio: bg_speed, positionOffset: [1500, Screen.height * 0.9])
+
+    schedule_update do |dt|
+      background_scroll_velocity = jbp(-1000, 0)
+      @background_node.position = jbpAdd(@background_node.position, jbpMult(background_scroll_velocity, dt));
+    end
   end
 end
